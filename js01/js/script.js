c7eb50612088a6af57050ef83d8556f67baaf0c7
Ruslan Dzhetere <rdzhetere@gmail.com>
2016-04-24 20:05:58 +0300

    js01

--- js01/js/script.js
+++ js01/js/script.js
@@ -1,17 +1,21 @@
 function pow(x,n) {
   var result;
   if (x == 0 && n >= 0) {
-    flag = true;
     return x;
   } else if (x != 0 && n == 0) {
     result = 1;
-    flag = true;
     return result;
-  } else {
+  } else if (x !=0 && n < 0) {
+    result = 1;
+    for (var i = 0; i < Math.abs(n); i++) {
+      result = result * x;
+    }
+    return 1/result;
+  }
+  else {
     result = 1;
     for (var i = 0; i < n; i++) {
       result = result * x;
-      flag = true;
     }
     return result;
   }
@@ -26,8 +30,4 @@ var exponent = prompt('Введите степень числа');
 }
 var flag = false;
 var result = pow(base,exponent);
-if (flag == true){
 console.log('Результат = ' + result);
-} else {
-console.log('Степень отрицательного числа не поддерживается');
-}
